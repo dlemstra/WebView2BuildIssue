20fb994160684e463b9dddc96e3057602a112984
Dirk Lemstra <dirk@lemstra.org>
2025-05-15 11:17:39 +0200

    Created sample project that demonstrates a build issue with Microsoft.Web.WebView2.

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WebView2BuildIssue;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
